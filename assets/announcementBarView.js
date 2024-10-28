bfcbf83f897996614d78aa5010a5488870a9664c
Nikhil Gupta <nickrick028@gmail.com>
2024-10-29 02:58:54 +0530

    Fixes for announcement bar and product page

--- assets/announcementBarView.js
+++ assets/announcementBarView.js
@@ -1,4 +1,22 @@
     document.addEventListener('DOMContentLoaded', function() {
+      /*
+      const div = document.querySelector('.scrollable-text');
+      console.log("inner ", div.innerText)
+      div.innerText = div.innerText.replace(/\n/g, ' ');
+
+      // Optionally, add a CSS style to the div to handle overflow
+      div.style.overflow = 'hidden';
+      div.style.textOverflow = 'ellipsis'; // Optional: Adds ellipsis (...) for truncated text
+      div.style.display = 'inline-block';  // Ensures the content behaves like inline text
+      div.style.whiteSpace = 'nowrap';     // Forces the content to remain on one line
+      */
+     const div = document.querySelector('.bb-max-width');
+
+     const height = div.offsetHeight;
+     
+     console.log("Height of the div:", height, "px");
+     document.documentElement.style.setProperty('--announcement-bar-height', `${height / 5}px`);
+
       const spanTextElements = document.querySelectorAll('.scrollable-text');
       const scrollingContainers = document.querySelectorAll('.scrolling-text-container');
     
