17dba0bce5b906f077089dbcb2040c9f612fb22b
Sawantkun <sawantkumar199@gmail.com>
2025-01-17 16:16:07 +0530

    text alignment

--- assets/icon-with-text.js
+++ assets/icon-with-text.js
@@ -148,7 +148,7 @@ document.addEventListener("DOMContentLoaded", () => {
             console.log("Showing navigation elements");
             arrows.forEach(arrow => arrow.style.display = "flex");
             if (pagination) pagination.style.display = "flex";
-            if (mainContainer) mainContainer.style.textAlign = "center";
+            if (mainContainer) mainContainer.style.textAlign = "start";
         }
     }
 
